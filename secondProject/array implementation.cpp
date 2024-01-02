cb68897372b4e1797f5baec6d4441d82603eed29
za1arya <zakaryayezdanpanah@gmail.com>
2024-01-03 01:23:01 +0330

    adding array implemetation into secondProject

---
+++ secondProject/array implementation.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+class array
+{
+    private:
+
+    int sizeCounter =0;
+    int array[100];
+
+    public:
+
+     void insertAtIndex(int,int);
+     int deleteAtIndex(int);
+     int findIndexByValue(int);
+
+};
+
+void array ::insertAtIndex(int index,int value)
+{
+    for(int i=sizeCounter;i>=index;i--)
+    {
+       this->array[i+1]=this->array[i]; 
+    }
+    this->array[index]=value;
+    sizeCounter++;
+
+}
+
+int array::deleteAtIndex(int index)
+{
+    int temp = this->array[index];
+
+   for(int i=index;i<= sizeCounter;i++)
+   {
+    this->array[i]=this->array[i+1];
+   }
+   sizeCounter--;
+
+   return temp;
+};
+
+int array::findIndexByValue(int value)
+{
+    int counter=0;
+    for(int counter=sizeCounter;counter>=0;counter--)
+    {
+        if(this->array[counter]==value)
+        return counter;
+    }
+    return 0;
+}
+
